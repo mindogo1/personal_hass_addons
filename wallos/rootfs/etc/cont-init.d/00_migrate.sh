427c68449ee53cbf4f58349f889e020926939944
mindogo1 <domarkas.mindaugas@gmail.com>
2025-07-03 00:11:55 +0300

    readme

--- wallos/rootfs/etc/cont-init.d/00_migrate.sh
+++ wallos/rootfs/etc/cont-init.d/00_migrate.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/with-contenv bash
 set -e
 
-# Ensure persistence dirs
+# Create persistence dirs
 mkdir -p /data/db
 mkdir -p /data/images/uploads/logos
 
-# Migrate existing DB
+# Migrate existing DB files
 if [ -d /var/www/html/db ] && [ "$(ls -A /var/www/html/db)" ]; then
   mv /var/www/html/db/* /data/db/
 fi
@@ -19,4 +19,4 @@ fi
 rm -rf /var/www/html/images/uploads/logos
 ln -s /data/images/uploads/logos /var/www/html/images/uploads/logos
 
-echo "✔️ Wallos data migrated to /data"
+echo "✅ Wallos data migrated to /data"
