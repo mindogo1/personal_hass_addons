f5ab0fceea4f3c1613d94d905dd453d72519d02a
mindogo1 <domarkas.mindaugas@gmail.com>
2025-07-02 14:33:50 +0300

    adding test

--- cleanuparr/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export RADARR_URL="${RADARR_URL}"
-export RADARR_API_KEY="${RADARR_API_KEY}"
-export SONARR_URL="${SONARR_URL}"
-export SONARR_API_KEY="${SONARR_API_KEY}"
-
-/cleanuparr/cleanuparr
