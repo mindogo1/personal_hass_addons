cd1e30cb4e907c06567a88b3aaa2852dd1bd054b
mindogo1 <domarkas.mindaugas@gmail.com>
2025-07-02 23:54:50 +0300

    readme

--- wallos/rootfs/etc/cont-init.d/00_migrate.sh
+++ wallos/rootfs/etc/cont-init.d/00_migrate.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
+# s6 cont-init.d script for data persistence
 set -e
 
+# Migrate DB
 mkdir -p /data/db
-mkdir -p /data/images/uploads/logos
-
 if [ -d /var/www/html/db ] && [ "$(ls -A /var/www/html/db)" ]; then
   mv /var/www/html/db/* /data/db/
 fi
+rm -rf /var/www/html/db
+ln -s /data/db /var/www/html/db
+
+# Migrate logos
+mkdir -p /data/images/uploads/logos
 if [ -d /var/www/html/images/uploads/logos ] && [ "$(ls -A /var/www/html/images/uploads/logos)" ]; then
   mv /var/www/html/images/uploads/logos/* /data/images/uploads/logos/
 fi
-
-rm -rf /var/www/html/db
-ln -s /data/db /var/www/html/db
 rm -rf /var/www/html/images/uploads/logos
 ln -s /data/images/uploads/logos /var/www/html/images/uploads/logos
 
-exit 0
+echo "✔️ Migrated Wallos data to /data"
