b4ab545343081f9135e6f0034a57bb1e9185a4fd
mindogo1 <domarkas.mindaugas@gmail.com>
2025-07-02 22:38:27 +0300

    readme

--- wallos/run.sh
+++ wallos/run.sh
@@ -1,27 +1,17 @@
 #!/usr/bin/env bash
 set -e
-
-# Prepare persistent folders under addon_config
-mkdir -p /addon_config/db
-mkdir -p /addon_config/logos
-
-# Migrate existing DB files
+# Prepare persistence
+mkdir -p /config/db /config/images/uploads/logos
+# Migrate existing data
 if [ -d /var/www/html/db ] && [ "$(ls -A /var/www/html/db)" ]; then
-  mv /var/www/html/db/* /addon_config/db/
+  mv /var/www/html/db/* /config/db/
 fi
-
-# Migrate existing logos
 if [ -d /var/www/html/images/uploads/logos ] && [ "$(ls -A /var/www/html/images/uploads/logos)" ]; then
-  mv /var/www/html/images/uploads/logos/* /addon_config/logos/
+  mv /var/www/html/images/uploads/logos/* /config/images/uploads/logos/
 fi
-
-# Symlink persistent storage
+# Symlink persistence
 rm -rf /var/www/html/db
-ln -s /addon_config/db /var/www/html/db
-
+ln -s /config/db /var/www/html/db
 rm -rf /var/www/html/images/uploads/logos
-ln -s /addon_config/logos /var/www/html/images/uploads/logos
-
-echo "Wallos data directories mounted"
-
+ln -s /config/images/uploads/logos /var/www/html/images/uploads/logos
 exit 0
